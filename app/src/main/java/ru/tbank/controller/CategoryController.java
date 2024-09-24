94695f314c7772bbc91984f236a070cc55bbe06e
Oleg Bondarev <mirpribili@ya.ru>
2024-09-24 15:06:59 +0300

    first Aspect

---
+++ app/src/main/java/ru/tbank/controller/CategoryController.java
@@ -0,0 +1,58 @@
+package ru.tbank.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+//import ru.tbank.annotation.LogExecutionTime;
+import ru.tbank.annotation.LogControllerExecution;
+import ru.tbank.model.Category;
+import ru.tbank.repository.CategoryRepository;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/api/v1/places/categories")
+//@LogExecutionTime
+public class CategoryController {
+    private final CategoryRepository repository;
+
+    public CategoryController(CategoryRepository repository) {
+        this.repository = repository;
+    }
+    @LogControllerExecution
+    @GetMapping
+    public Collection<Category> getAllCategories() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Category> getCategoryById(@PathVariable int id) {
+        Category category = repository.findById(id);
+        if (category == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(category);
+    }
+
+    //@LogExecutionTime
+    @PostMapping
+    public ResponseEntity<Category> createCategory(@RequestBody Category category) {
+        Category createdCategory = repository.save(category);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdCategory);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Category> updateCategory(@PathVariable int id, @RequestBody Category category) {
+        Category updatedCategory = repository.update(id, category);
+        if (updatedCategory == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(updatedCategory);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCategory(@PathVariable int id) {
+        repository.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ app/src/main/java/ru/tbank/model/Category.java
@@ -0,0 +1,14 @@
+package ru.tbank.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+    private int id;
+    private String slug;
+    private String name;
+}
