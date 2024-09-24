fb19e0ca1ae4feedc81218f1b0ca824a4b726642
Oleg Bondarev <mirpribili@ya.ru>
2024-09-24 11:52:34 +0300

    fix

--- app/src/main/java/ru/tbank/App.java
+++ app/src/main/java/ru/tbank/App.java
@@ -12,5 +12,6 @@ public class App {
     public static void main(String[] args) {
         logger.info("Запуск приложения...");
         SpringApplication.run(App.class, args);
+        // http://localhost:8080/api/v1/places/categories
     }
 }
