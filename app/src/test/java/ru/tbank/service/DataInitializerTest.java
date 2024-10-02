07dcf727e0e22d62fcf6f65d9304dd2dd2c72034
Oleg Bondarev <mirpribili@ya.ru>
2024-10-02 20:14:16 +0300

    add integration test for DataInitializer

--- app/src/test/java/ru/tbank/service/DataInitializerTest.java
+++ app/src/test/java/ru/tbank/service/DataInitializerTest.java
@@ -59,5 +59,72 @@ class DataInitializerTest {
         verify(locationRepository, times(2)).save(any(Location.class));
     }
 
-    // Другие тесты...
+    @Test
+    void testInit_EmptyCategoriesResponse() {
+        when(restTemplate.getForObject(baseUrl + "/place-categories", Category[].class)).thenReturn(new Category[0]);
+
+        Location[] locations = {
+                new Location(1, "location-1", "Город 1"),
+                new Location(2, "location-2", "Город 2")
+        };
+        when(restTemplate.getForObject(baseUrl + "/locations", Location[].class)).thenReturn(locations);
+
+        dataInitializer.init();
+
+        verify(categoryRepository, never()).save(any(Category.class)); // Не должно быть сохранений категорий
+        verify(locationRepository, times(2)).save(any(Location.class)); // Должны сохраниться города
+    }
+
+    @Test
+    void testInit_EmptyLocationsResponse() {
+        Category[] categories = {
+                new Category(1, "category-1", "Категория 1"),
+                new Category(2, "category-2", "Категория 2")
+        };
+        when(restTemplate.getForObject(baseUrl + "/place-categories", Category[].class)).thenReturn(categories);
+
+        when(restTemplate.getForObject(baseUrl + "/locations", Location[].class)).thenReturn(new Location[0]);
+
+        dataInitializer.init();
+
+        verify(categoryRepository, times(2)).save(any(Category.class)); // Должны сохраниться категории
+        verify(locationRepository, never()).save(any(Location.class)); // Не должно быть сохранений городов
+    }
+
+    @Test
+    void testInit_ExceptionDuringCategoriesLoad() {
+        when(restTemplate.getForObject(baseUrl + "/place-categories", Category[].class))
+                .thenThrow(new RestClientException("Ошибка при загрузке категорий"));
+
+        Location[] locations = {
+                new Location(1, "location-1", "Город 1"),
+                new Location(2, "location-2", "Город 2")
+        };
+        when(restTemplate.getForObject(baseUrl + "/locations", Location[].class)).thenReturn(locations);
+
+        dataInitializer.init();
+
+        verify(categoryRepository, never()).save(any(Category.class)); // Категории не должны быть сохранены
+
+        verify(locationRepository, times(2)).save(any(Location.class)); // Города должны сохраниться несмотря на исключение с категориями
+    }
+
+    @Test
+    void testInit_ExceptionDuringLocationsLoad() {
+        Category[] categories = {
+                new Category(1, "category-1", "Категория 1"),
+                new Category(2, "category-2", "Категория 2")
+        };
+
+        when(restTemplate.getForObject(baseUrl + "/place-categories", Category[].class)).thenReturn(categories);
+
+        when(restTemplate.getForObject(baseUrl + "/locations", Location[].class))
+                .thenThrow(new RestClientException("Ошибка при загрузке городов"));
+
+        dataInitializer.init();
+
+        verify(locationRepository, never()).save(any(Location.class)); // Города не должны быть сохранены
+
+        verify(categoryRepository, times(2)).save(any(Category.class)); // Категории должны сохраниться несмотря на исключение с городами
+    }
 }
