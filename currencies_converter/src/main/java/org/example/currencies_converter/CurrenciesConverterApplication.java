31817845522c661508af31d040aba1206a10c4e7
Oleg Bondarev <mirpribili@ya.ru>
2024-10-08 19:03:16 +0300

    init currencies convert

---
+++ currencies_converter/src/main/java/org/example/currencies_converter/CurrenciesConverterApplication.java
@@ -0,0 +1,13 @@
+package org.example.currencies_converter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrenciesConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrenciesConverterApplication.class, args);
+	}
+
+}
---
+++ currencies_converter/src/test/java/org/example/currencies_converter/CurrenciesConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.currencies_converter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrenciesConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
